9f58a79135aa08ab24826692fe2ea8da927500c0
Jimmyh091 <jaimecaballero164@gmail.com>
2024-12-12 17:06:45 +0100

    cosas

---
+++ app/src/main/java/com/example/practica_trones_pm_o_di_nose/Calculatron.kt
@@ -0,0 +1,20 @@
+package com.example.practica_trones_pm_o_di_nose
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Calculatron : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_calculatron)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}

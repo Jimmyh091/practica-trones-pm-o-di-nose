292e19f387889be77dc10a541b69ff3c4b73c3b4
Jimmyh091 <jaimecaballero164@gmail.com>
2024-12-12 20:57:55 +0100

    guardar

--- app/src/main/java/com/example/practica_trones_pm_o_di_nose/Calculatron.kt
+++ app/src/main/java/com/example/practica_trones_pm_o_di_nose/Calculatron.kt
@@ -42,6 +42,79 @@ class Calculatron : AppCompatActivity() {
             insets
         }
 
+        bind.boton0.setOnClickListener {
+            bind.input.setText("0")
+        }
+        bind.boton1.setOnClickListener {
+            bind.input.setText("1")
+        }
+        bind.boton2.setOnClickListener {
+            bind.input.setText("2")
+        }
+        bind.boton3.setOnClickListener {
+            bind.input.setText("3")
+        }
+        bind.boton4.setOnClickListener {
+            bind.input.setText("4")
+        }
+        bind.boton5.setOnClickListener {
+            bind.input.setText("5")
+        }
+        bind.boton6.setOnClickListener {
+            bind.input.setText("6")
+        }
+        bind.boton7.setOnClickListener {
+            bind.input.setText("7")
+        }
+        bind.boton8.setOnClickListener {
+            bind.input.setText("8")
+        }
+        bind.boton9.setOnClickListener {
+            bind.input.text="9"
+        }
+        bind.botonC.setOnClickListener {
+            bind.input.text = ""
+        }
+        bind.botonCE.setOnClickListener {
+            bind.input.text = ""
+        }
+        bind.menos.setOnClickListener {
+            bind.input.setText("-")
+        }
+        bind.botonIgual.setOnClickListener {
+            pasarTurno()
+            generarCuenta()
+        }
 
+    }
+
+    fun generarCuenta() : List<Int> {
+
+        numero1 = (min..max).random()
+        numero2 = (min..max).random()
+        operador = (0..2).random()
+
+        return listOf(numero1, numero2, operador)
+    }
+
+    fun pasarTurno(){
+        var correcto : Boolean = true
+
+        when(operador){
+            0 -> correcto = resultado == numero1 + numero2
+            1 -> correcto = resultado == numero1 - numero2
+            2 -> correcto = resultado == numero1 * numero2
+        }
+
+        if(correcto){
+            aciertos++
+            bind.correccion.setImageResource(R.drawable.correcto)
+        }else{
+            fallos++
+            bind.correccion.setImageResource(R.drawable.incorrecto)
+        }
+
+        cuentaPasada = cuentaActual
+        cuentaActual = cuentaSiguiente
     }
 }

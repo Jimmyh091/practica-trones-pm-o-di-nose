7d9d5ec2f8ac5a4fc550ec5a7938dc40a7c4b262
Jimmyh091 <jaimecaballero164@gmail.com>
2024-12-12 20:14:55 +0100

    AYUDA

--- app/src/main/java/com/example/practica_trones_pm_o_di_nose/Calculatron.kt
+++ app/src/main/java/com/example/practica_trones_pm_o_di_nose/Calculatron.kt
@@ -5,16 +5,43 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.practica_trones_pm_o_di_nose.databinding.ActivityCalculatronBinding
 
 class Calculatron : AppCompatActivity() {
+
+    private lateinit var bind : ActivityCalculatronBinding
+
+    private var max : Int = resources.getInteger(R.integer.maximo)
+    private var min : Int = resources.getInteger(R.integer.minimo)
+    private var cuentaAtras : Int = resources.getInteger(R.integer.cuentaatras)
+    private var suma : Boolean = resources.getBoolean(R.bool.suma)
+    private var resta : Boolean = resources.getBoolean(R.bool.resta)
+    private var multiplicacion : Boolean = resources.getBoolean(R.bool.multiplicacion)
+
+    private var aciertostotales : Int = 0
+    private var fallostotales : Int = 0
+    private var aciertos : Int = 0
+    private var fallos : Int = 0
+
+    private var cuentaPasada : String = ""
+    private var cuentaActual : String = ""
+    private var cuentaSiguiente : String = ""
+
+    private var numero1 : Int = 0
+    private var numero2 : Int = 0
+    private var operador : Int = 0
+    private var resultado : Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_calculatron)
+        setContentView(bind.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
     }
 }
